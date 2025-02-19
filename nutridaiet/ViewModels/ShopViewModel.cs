7f19f17b01f24451da4ec6e9a302ba56b323d974
br3slow <breezeblow@outlook.com>
2025-02-19 22:48:36 +0800

    update: integrate AsyncImageLoader and load products from JSON

--- nutridaiet/ViewModels/ShopViewModel.cs
+++ nutridaiet/ViewModels/ShopViewModel.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
 using Avalonia.SimpleRouter;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
+using System.IO;
+using System.Text.Json;
 using nutridaiet.Models;
 
 namespace nutridaiet.ViewModels;
@@ -16,31 +19,12 @@ public partial class ShopViewModel : ViewModelBase
     public ShopViewModel(HistoryRouter<ViewModelBase> router)
     {
         _router = router;
-        // 初始化商品数据（后续可以从在线API获取）
-        Products = new ObservableCollection<Product>
-        {
-            new Product
-            {
-                Name = "羽衣甘蓝",
-                Description = "根据您视力情况推荐",
-                Price = 166.9m,
-                ImageUrl = "https://pic.nximg.cn/file/20230820/34599220_200841306105_2.jpg"
-            },
-            new Product
-            {
-                Name = "有机苹果",
-                Description = "新鲜采摘，健康美味",
-                Price = 99.9m,
-                ImageUrl = "https://th.bing.com/th/id/OIP.kzo22vGPqcf7d5w_QJhrfQHaFj?rs=1&pid=ImgDetMain"
-            },
-            new Product
-            {
-                Name = "有机苹果",
-                Description = "新鲜采摘，健康美味",
-                Price = 99.9m,
-                ImageUrl = "https://th.bing.com/th/id/OIP.kzo22vGPqcf7d5w_QJhrfQHaFj?rs=1&pid=ImgDetMain"
-            },
-        };
+
+        var json = File.ReadAllText("C:\\Users\\breeze\\RiderProjects\\nutri-daiet-app\\nutridaiet\\Assets\\Products.json");
+
+        var products = JsonSerializer.Deserialize<List<Product>>(json);
+
+        Products = new ObservableCollection<Product>(products);
     }
 
 
